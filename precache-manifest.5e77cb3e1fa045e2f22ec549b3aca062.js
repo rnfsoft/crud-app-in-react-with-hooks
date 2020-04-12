146c49731057280a2b07a9dcb95746e4e4ca788b
Tae Hee Choi <tchoi@nitinol.internal>
2020-04-12 11:01:26 -0700

    Updates

---
+++ precache-manifest.5e77cb3e1fa045e2f22ec549b3aca062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2d84719233bbb20e3f68502f8df888",
+    "url": "/crud-app-in-react-with-hooks/index.html"
+  },
+  {
+    "revision": "0be44001cb4c907ea0ad",
+    "url": "/crud-app-in-react-with-hooks/static/css/main.db2fbb6c.chunk.css"
+  },
+  {
+    "revision": "661f291f67b6705e23c0",
+    "url": "/crud-app-in-react-with-hooks/static/js/2.9a4006ae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/crud-app-in-react-with-hooks/static/js/2.9a4006ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be44001cb4c907ea0ad",
+    "url": "/crud-app-in-react-with-hooks/static/js/main.3e023eb7.chunk.js"
+  },
+  {
+    "revision": "f63c668d70c43bdeeeb6",
+    "url": "/crud-app-in-react-with-hooks/static/js/runtime-main.41fba4db.js"
+  }
+]);
